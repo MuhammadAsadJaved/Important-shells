c96f5697519a029fafc22ee888ac0c2eac7ea724
MuhammadAsadJaved <asadjavedgujar@gmail.com>
2019-11-22 17:38:15 +0800

    Update draw_bouding_box_on_single_image.py

--- draw_bouding_box_on_single_image.py
+++ draw_bouding_box_on_single_image.py
@@ -1,50 +1,25 @@
 
 """
 Draw bounding box on single image according to it's corresponding xml's information to check 
-weather label information is right or wrong.
+weather label information is right or wrong , This script is to draw just one bounding box, for all boundix boxes use 
+the other file in the repo.
 """
 import cv2
-import xml.etree.ElementTree as ET
 import glob
 import os
 import numpy as np
-xmlFile = "001.xml"  # input xml name
-imgFile =cv2.imread('001.jpg')  # input image name 
-xmin =[]
-xmax =[]
-ymin =[]
-ymax = []
-classes =[]
-tree = ET.parse(xmlFile)
-root = tree.getroot()
-for obj in root.findall("object"):
-    bndbox = obj.find("bndbox")
-    classes.append(obj.find("name").text)
-    xmin.append(int(bndbox.find("xmin").text))
-    xmax.append(int(bndbox.find("xmax").text))
-    ymin.append(int(bndbox.find("ymin").text))
-    ymax.append(int(bndbox.find("ymax").text))
-count = 0
 
-#y= int(len(xmax))
-n = 0
-#for n in xmin:
-while n<= len(xmax)-1:
-    cv2.rectangle(imgFile, (xmin[n], ymin[n]), (xmax[n], ymax[n]), (255, 255, 0), 2)
-   # print(n)
-    n+=1
-#for n in range(xmin.len()):
-<xmin>310</xmin><xmax>381</xmax><ymin>160</ymin><ymax>237</ymax>
-
-cv2.imwrite("rsults.jpg", imgFile)
+im =cv2.imread('MVI_1532_NIR_frame25.jpg')   #Image path and name
 
 
-#xmin =310
-#xmax =153
-#ymin =160
-#ymax =237
-#cv2.rectangle(im, (xmin, ymin), (xmax, ymax), (255,255,0),2)
-#cv2.imwrite("test.jpg",im)
+#start and end points of bounding box,  it may used from the xml
+xmin =365
+xmax =420
+ymin =505
+ymax =543
+cv2.rectangle(im, (xmin, ymin), (xmax, ymax), (255,255,0),2)
+
+cv2.imwrite("test.jpg",im)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
