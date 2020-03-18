a76e796574e8d0996e088d70a1cd1a042187232e
Asad <asadjavedgujar@gmail.com>
2020-03-18 16:40:29 +0800

    Update deleteObject_coco.py

--- deleteObject_coco.py
+++ deleteObject_coco.py
@@ -21,13 +21,22 @@ for dirpaths, dirnames, filenames in os.walk(origin_ann_dir):
       for obj in root.findall('object'):
         name = str(obj.find('name').text)
  
-        if (name == "person" or  name == "bicycle" or name == "car" or name == "motorbike" or name == "bus" or name == "truck"):
-          #root.remove(obj) 
-          a=11
-        else:
-          root.remove(obj) 
-       # if (name == "aeroplane" or  name == "train" and name == "boat" and name != "motorbike" and name != "bus" and name != "truck"):
-        #  root.remove(obj)
+#The first method is you can mention the objects you want to remove.
+
+        #if (name == "tricycle" ):
+        #  root.remove(object)
+        #if (name == "bicycle" ):
+        #  root.remove(object)
+        #if (name == "awning-tricyle" ):
+        #  root.remove(object)
+        #if (name == "motor" ):
+        #  root.remove(object)
+
+ #The second method is you can metion the objects you want to keep and remove rest of them.
+        
+        if (name != "person" and  name != "bicycle" and name != "car" and name != "motor" and name != "bus"):
+          root.remove(object)
+          count+=1
   
       tree.write(new_ann_path)
 
