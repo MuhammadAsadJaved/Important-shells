73d6dc20c4df9741bb4152277086966a671c4ccd
Asad <asadjavedgujar@gmail.com>
2020-03-19 16:16:48 +0800

    Add files via upload

---
+++ Ruyi-convert-jpg2bgr.py
@@ -0,0 +1,27 @@
+import cv2
+#path in windows
+imgpath = "C:\\Users\\Administrator\\Desktop\\Asad\\1.jpg"
+saveimg = r"C:\\Users\\Administrator\\Desktop\\Asad\\1.bgr"
+#path in linux
+#imgpath = "./images/1.jpg"
+#saveimg = r"./images/1.bgr"
+img = cv2.imread(imgpath)
+
+save_img_size = 608
+
+if img is None:
+    print("img is none")
+else:
+    img = cv2.resize(img,(save_img_size,save_img_size))
+    (B, G, R) = cv2.split(img)
+    with open(saveimg,'wb')as fp:
+        for i in range(save_img_size):
+            for j in range(save_img_size):
+                fp.write(B[i, j])
+        for i in range(save_img_size):
+            for j in range(save_img_size):
+                fp.write(G[i, j])
+        for i in range(save_img_size):
+            for j in range(save_img_size):
+                fp.write(R[i, j])
+    print("save success")
---
+++ Ruyi-convert-jpg2yuv.cpp
@@ -0,0 +1,53 @@
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+#include <opencv2/imgproc/imgproc_c.h>
+
+using namespace cv;
+
+int main(int argv, char **argc)
+{
+	IplImage *pstImage = NULL;
+	IplImage *pstYUVImage = NULL;
+	FILE *fp = NULL;
+
+	pstImage = cvLoadImage("C://Users/Administrator/Desktop/Asad/FaceNew/5.jpg", CV_LOAD_IMAGE_COLOR);
+	fp = fopen("C://Users/Administrator/Desktop/Asad/FaceNew/5.yuv", "wb");
+	pstYUVImage = cvCreateImage(cvSize(pstImage->width, pstImage->height), IPL_DEPTH_8U, 3);
+
+	cvCvtColor(pstImage, pstYUVImage, CV_BGR2YUV);
+
+	for (int i = 0; i < pstImage->width * pstImage->height; i++)
+	{
+		//提取Y分量
+		fwrite(&pstYUVImage->imageData[i * 3], 1, 1, fp);
+		//提取U分量
+		//fwrite(&pstYUVImage->imageData[i*3+2], 1 , 1, fp);
+		//提取V分量
+		//fwrite(&pstYUVImage->imageData[i*3+1], 1 , 1, fp);
+	}
+
+	for (int i = 0; i < pstImage->height; i = i + 2)
+	{
+		for (int j = 0; j < pstImage->width; j = j + 2)
+		{
+			//提取U分量
+			fwrite(&pstYUVImage->imageData[3 * (i*pstImage->width + j) + 2], 1, 1, fp);
+		}
+	}
+
+	for (int i = 0; i < pstImage->height; i = i + 2)
+	{
+		for (int j = 0; j < pstImage->width; j = j + 2)
+		{
+			//提取V分量
+			fwrite(&pstYUVImage->imageData[3 * (i*pstImage->width + j) + 1], 1, 1, fp);
+		}
+	}
+
+	cvShowImage("Win", pstImage);
+
+	cvWaitKey(0);
+	cvReleaseImage(&pstImage);
+	cvReleaseImage(&pstYUVImage);
+	fclose(fp);
+	return 0;
