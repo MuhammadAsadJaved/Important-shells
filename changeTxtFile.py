29543871839e9298c71656569f008a4bc8b27dc8
Asad <asadjavedgujar@gmail.com>
2020-05-11 12:46:15 +0800

    Add files via upload

---
+++ changeTxtFile.py
@@ -0,0 +1,48 @@
+
+import os
+files_dir = './f/'
+
+out_dir = './out/'
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
+files_list = os.listdir(files_dir)
+
+out_files_list = os.listdir(out_dir)
+
+for files in files_list:
+    file = files_dir+files
+    outputfile = out_dir+files
+
+    with open(file) as in_file, open(outputfile, 'w') as out_file:
+
+
+        data = in_file.read()
+        data = data.replace(' ', '')
+        data = data.replace('(', '')
+        data = data.replace(')', '')
+        data = data.replace(',', ' ')
+        out_file.write(data)
+        #print(out_file)
+
+out_dir2 = './out2/'
+if not os.path.exists(out_dir2):
+    os.mkdir(out_dir2)
+
+
+for files in out_files_list:
+    file2 = out_dir+files
+    #print(file2)
+    outputfile = out_dir2+files
+    with open(file2) as in_file, open(outputfile, 'w') as out_file:
+
+        for line in in_file:
+            line = line.strip()
+            a = (line[0:-1])
+            b = (line[-1])
+            # c = str.join(b,a)
+            c = " ".join((b, a))
+            #print(c)
+            out_file.write(c)
+            out_file.write('\n')
+           # print(out_file)
+#
