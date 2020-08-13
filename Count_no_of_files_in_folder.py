db01a2095af88b174537a9c062e4eaa9fa06bf1b
Asad <asadjavedgujar@gmail.com>
2020-08-13 15:55:23 +0800

    Update Count_no_of_files_in_folder.py

--- Count_no_of_files_in_folder.py
+++ Count_no_of_files_in_folder.py
@@ -8,3 +8,14 @@ Filespath = "CroppedFinalImgs"  #Input directory
 no =len(os.listdir(Filespath))
 
 print(no)
+
+
+'''
+Or use this shell command
+
+#Count number of files/images in folder and it's subfolders 
+
+$find sequences -type f | wc -l                  #sequences is folder name.
+
+
+'''
